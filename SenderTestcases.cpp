df5ef52d567b346f8c4558292664700d7780ce60
Akshaya2807 <115683252+Akshaya2807@users.noreply.github.com>
2023-02-20 15:47:11 +0530

    Update SenderTestcases.cpp

--- SenderTestcases.cpp
+++ SenderTestcases.cpp
@@ -1,6 +1,6 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 
-#include "test/catch.hpp"
+
 #include "Sender.c"
 
 TEST_CASE("positive case: test1 - process input sensors value and send to receiver") {
