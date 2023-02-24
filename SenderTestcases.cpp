a2f54bde202fe4f43af63ad33ca39a6b0041d349
Akshaya2807 <115683252+Akshaya2807@users.noreply.github.com>
2023-02-24 10:53:00 +0530

    Update SenderTestcases.cpp

--- SenderTestcases.cpp
+++ SenderTestcases.cpp
@@ -1,8 +1,12 @@
-#include "Sender.h"
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+
+#include "test/catch.hpp"
+#include "Sender.c"
+
 
 void TEST_CASE_01(){
   
-  BMSData_s sensorData_s;
+ BMSData_s sensorData_s;
 
   sensorData_s.chargeRate_minvalue = 0;
   sensorData_s.chargeRate_maxvalue = 100;
